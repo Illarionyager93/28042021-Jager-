b7cc898a8e272a0d6c7042fa596a55c7d80bb31d
Illarionyager93 <illarionyager93@gmail.com>
2021-05-26 08:28:18 -0700

    multidimensional array #1

---
+++ H08/Home_8.php
@@ -0,0 +1,92 @@
+<?php
+
+$MySite2 = [
+    ['title' => 'index', 'link' => [
+        ['title' => 'account', 'link' =>
+            ['title' => 'SingIn', 'link' => '/SingIn-link'],
+            ['title' => 'Register', 'link' => '/Register-link'],
+            ['title' => 'ForgetPassword', 'link' => '/ForgetPassword-link'],
+        ],
+        ['title' => 'search', 'link' =>
+            ['title' => 'BasicSearch', 'link' => '/BasicSearch-link'],
+            ['title' => 'AdvancedSearch', 'link' => '/AdvancedSearch-link'],
+        ],
+        ['title' => 'catalog', 'link' =>
+            ['title' => 'SubCategory', 'link' => '/SubCategory-link'],
+            ['title' => 'product', 'link' => '/product-link'],
+        ],
+    ]],
+];
+
+/*
+ * Your PHP - code here
+ */
+print_r($MySite2);
+//foreach ($MySite2 as $key => $value) {
+//    Echo "{$key}: {$value}<br>";
+//}
+foreach ($MySite2 as $key => $value) {
+    Echo "{$key}: {$value}<br>";
+
+}
+
+
+
+
+//function arrprint ($arr) {
+//        foreach($arr as $key => $v) {
+////            print $key . '$v '.'<br>';
+//        if (is_array($v)) {//если значение = массив то
+//            print '<br>'; //пустая строчка
+//            arrprint($v);//рекусия функции/углубляемся
+//        } else {
+//            print $v .'<br>';//вывод значения
+//
+//        }
+//        }
+//
+//}
+
+//arrprint($MySite2);
+
+
+
+
+/*
+<ul>
+    <li><a href="/link">TITLE</a></li>
+    <li><a href="/link2">TITLE2</a></li>
+    <li>
+PARENT 1
+<ul>
+            <li><a href="/child-link">CHILD</a></li>
+            <li><a href="/child-link2">CHILD2</a></li>
+        </ul>
+    </li>
+</ul>
+*/
+
+
+//
+//$a1 = [
+//    'name' => 'Dmytro',
+//    'lastName' => 'Dmytro',
+//    'age' => 32,
+//    'skills' => [
+//        'PHP',
+//        'JS'
+//    ],
+//];
+//
+//foreach ($a1 as $key => $value) {
+//    if (is_array($value)) {
+//        $value = serialize($value);
+//    }
+//    echo "{$key}: {$value}<br>";
+//}
+//
+//
+//
+//
+//
+//var_dump($MySite2);
